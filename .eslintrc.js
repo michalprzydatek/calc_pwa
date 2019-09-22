0523c2a6afd8c95a3ca1adbcf0de9446b3bdf7b6
Michal Przydatek <michalprzydatek@gmail.com>
2019-09-22 22:08:25 +0200

    moveToRoot

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: 'NAZWA',
+
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    rules: {},
+    globals: {},
+    parserOptions: {}
+};
